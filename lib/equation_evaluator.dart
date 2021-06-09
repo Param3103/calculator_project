77bcd8a248fc77a3349d97584e9a2860faaac468
Param3103 <param.mehrotra@gmail.com>
2021-06-09 14:22:05 +0800

    need to troubleshoot compund equations

--- lib/equation_evaluator.dart
+++ lib/equation_evaluator.dart
@@ -1,7 +1,17 @@
 // we are given an equation in a string "12+4" and we have to evaluate it
+import 'dart:math';
 
 class Equation{
   static eval(expression) {
+    if ((expression.split("")).contains("(")){
+      //(2+3)*4
+      int left_index = expression.indexOf("(");
+      int right_index = expression.indexOf(")");
+      String new_exp = expression.substring(left_index + 1, right_index - 1);
+      String val_new_exp = eval(new_exp);
+      expression.replaceAll (new_exp, val_new_exp);
+      return(expression);
+    }
     if ((expression.split("")).contains("+")){
       // "21+4"
       num val = 0;
@@ -55,6 +65,20 @@ class Equation{
       String c = expression[1];
       String d = eval(c);
       val /= int.parse(d);
+      print(val.toString());
+      return(val.toString());
+    }
+    else if ((expression.split("")).contains("^")){
+      // "21-4"
+      num val = 0;
+      expression = expression.split("^");
+      //["21", "4"]
+      String a = expression[0];
+      String b = eval(a);
+      val += int.parse(b);
+      String c = expression[1];
+      String d = eval(c);
+      val = pow(val, int.parse(d));
       print(val.toString());
       return(val.toString());
     }
